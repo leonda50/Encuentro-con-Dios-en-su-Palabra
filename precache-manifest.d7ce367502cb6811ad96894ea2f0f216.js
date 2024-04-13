ee80988f384cf6b6124bed1b79fa6170b6812327
leonda50 <99834330+leonda50@users.noreply.github.com>
2024-04-12 20:11:57 -0500

    Add files via upload

---
+++ precache-manifest.d7ce367502cb6811ad96894ea2f0f216.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f094067060953f39ad1f5a92d507c8",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "444edf5c1e40118e56ce",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "859220d88feecbaf9919",
+    "url": "@yield('content-viewer-url')/static/css/main.5bb84e2c.chunk.css"
+  },
+  {
+    "revision": "444edf5c1e40118e56ce",
+    "url": "@yield('content-viewer-url')/static/js/2.f7603d50.chunk.js"
+  },
+  {
+    "revision": "cfb333affc1e9e4e40ea7a1e949500b6",
+    "url": "@yield('content-viewer-url')/static/js/2.f7603d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859220d88feecbaf9919",
+    "url": "@yield('content-viewer-url')/static/js/main.7151e120.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "@yield('content-viewer-url')/static/js/main.7151e120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33a12fe361989d2d462",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.eac69593.js"
+  },
+  {
+    "revision": "a754c5ed00d8f6cca2a96794a2beaddc",
+    "url": "@yield('content-viewer-url')/static/media/trophy.a754c5ed.svg"
+  }
+]);
